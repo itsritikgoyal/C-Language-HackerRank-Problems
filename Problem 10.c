3a0f3b20de662f53175224910c6407b43313a33b
Ritik Goyal <69449533+itsritikgoyal@users.noreply.github.com>
2021-03-01 11:27:00 +0530

    1D Array in C

---
+++ Problem 10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int sum = 0;
+    int *arr = (int*)malloc(n * sizeof(int));
+    for(int i = 0 ; i < n ; i++){
+        scanf("%d",&arr[i]);
+        sum += arr[i];
+    }free(arr);
+    printf("%d",sum);
+    return 0;
+}
